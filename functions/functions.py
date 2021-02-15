6fa8110fabd39b8f1ec1c957881e87e1f0ef82c2
Hillard Har <73195851+Hillard-har@users.noreply.github.com>
2021-02-15 21:45:08 +0530

    Update functions.py

--- functions/functions.py
+++ functions/functions.py
@@ -25,10 +25,10 @@ def sendMedia(update,context):
 def sendFile(update,context): 
 
 #To get the incoming file name 
- file_name = update.message.document.file_name
 
  try:
-  	update.message.reply_document(update.message.document.file_id,caption="*{} \n\n*@Anylink_Movies*".formate(file_name),parse_mode=ParseMode.MARKDOWN)
+        file_name = update.message.document.file_name
+  	update.message.reply_document(update.message.document.file_id,caption="*{} \n\n@Anylink_Movies*".formate(file_name),parse_mode=ParseMode.MARKDOWN)
  except Exception as e:
   	update.message.reply_text(e)
   	

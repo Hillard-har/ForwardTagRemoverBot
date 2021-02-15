12c6f94bd448a692fbf05220ad485ef6cf1f5780
Hillard Har <73195851+Hillard-har@users.noreply.github.com>
2021-02-15 22:05:20 +0530

    Update functions.py

--- functions/functions.py
+++ functions/functions.py
@@ -25,7 +25,7 @@ def sendMedia(update,context):
 def sendFile(update,context):  
  try:
    file_name = update.message.document.file_name
-   update.message.reply_document(update.message.document.file_id,caption="*{} \n\n@Anylink_Channels*".formate(file_name),parse_mode=ParseMode.MARKDOWN)
+   update.message.reply_document(update.message.document.file_id,caption="*{} \n\n@Anylink_Channels*".format(file_name),parse_mode=ParseMode.MARKDOWN)
  except Exception as e:
   	update.message.reply_text(e)
   	

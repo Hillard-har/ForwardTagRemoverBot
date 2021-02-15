ebfbc2bb0fe0f580f3ee1bfdf309106d3fa67bbc
Hillard Har <73195851+Hillard-har@users.noreply.github.com>
2021-02-15 19:35:11 +0530

    Update functions.py

--- functions/functions.py
+++ functions/functions.py
@@ -23,7 +23,7 @@ def sendMedia(update,context):
 #Function to send file
 def sendFile(update,context): 
  try:
-  	update.message.reply_document(update.message.document.file_id,caption=file_name)
+  	update.message.reply_document(update.message.document.file_id,caption="\n@Anylink_Movies")
  except Exception as e:
   	update.message.reply_text(e)
   	
